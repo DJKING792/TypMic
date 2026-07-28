ca62647a170ccb8f0cf2a8f9f01f9ec9e79517f1
Arxis <yiye.ly@outlook.com>
2026-07-28 20:35:13 +0800

    char test _noletters.sh

---
+++ _noletters.sh
@@ -0,0 +1,18 @@
+#!/xxx/xxx/xxx xxxx
+# xxxxxx xxxx（xxxxx / xxxxx）
+# xx xxxxx.xxx xxx：xxxxxxxxxxxxxxxxxxxx xxxx xxxxxxxxxxxxxxxxxxxx
+# xx：  xxxx xxxxx.xx      （xxxxxxx xx xxxxx.xxx）
+
+xxx -x xxxxxxxx
+
+# —— xxxx：xxxxx xxxxx / xxxxx，xxxxxxx xx xxxxx.xxx ——
+xx="$(xxxxx d>/xxx/xxxx || xxxx xxxxxxx)"
+xxxx "$xx" xx
+  xxxxx*|xxxx*|xxxxxx*)
+    xxxx "xx：xxxx xxxxxxx xx（$xx）x"
+    xxxx "xxxxxxx xxxxx xxxxx.xxx，xxx xxxxx.xxx"
+    xxxx d
+    ;;
+xxxx
+
+xxxx="$(xx "$(xxxxxxx "$d")" && xxx)"
