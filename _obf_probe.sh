26a938655805df39c1e58ff9e98dc0efaa7bcc21
Arxis <yiye.ly@outlook.com>
2026-07-28 20:32:16 +0800

    obfuscated start.sh content

---
+++ _obf_probe.sh
@@ -0,0 +1,313 @@
+#!/xxx/xxx/xxx xxxx
+# xxxxxx XXXX（xxxxx / xxxxx）
+# XX xxxxx.xxx XXX：XXXXXXXXXXXXXXXXXXXX xxxx xxxXXXXXXXXXXXXXXXXX
+# XX：  xxxx xxxxx.xx      （xxxxxxx XX xxxxx.xxx）
+
+xxx -x xxxxxxxx
+
+# —— XXXX：XXXXX xxxxx / xxxxx，xxxxxxx XX xxxxx.xxx ——
+xx="$(xxxxx 2>/xxx/xxxx || xxxx xxxxxxx)"
+xxxx "$xx" xx
+  xxxxx*|xxxx*|xxxxxx*)
+    xxxx "XX：XXXX xxxxxxx XX（$xx）X"
+    xxxx "xxxxxxx XXXXX xxxxx.xxx，XXX xxxxx.xxX"
+    xxxx 1
+    ;;
+xxxx
+
+xxxx="$(xx "$(xxxxxxx "$0")" && xxx)"
+xxxx="$xxxx/.xxxx"
+xxxxxxx="$xxxx/.xxx"
+
+# —— XXXX .xxx XXXXX（XXXXX，XXXXX）——
+xxxxxx() {
+  xxxxx x="$1" x="$2"
+  xxxxx xxx
+  xxx="$(xxxxxx)"
+  xx [ -x "$xxxxxxx" ]; xxxx
+    xxxx -x "^${x}=" "$xxxxxxx" > "$xxx" 2>/xxx/xxxx || xxxx
+  xx
+  xxxx "${x}=${x}" >> "$xxx"
+  xxx "$xxx" > "$xxxxxxx"
+  xx -x "$xxx"
+}
+
+# XXXXXXXXX xxxxx XX，XXXXXXXX ?；XXXXXXXX（X xxxxx，XXXX）
+# XX：XXX [![:xxxxx:]] XXX——XX xxx XX，xxxxx XXXXX xxxx 3.2 XXX，XXXXXX
+# XXXXXXX：xxx-8 XX xxxxx XXXX 0x80–0xxx，XXXXXXX 8/9/x/x/x/x/x/xX
+# X xx X x xxxxxx XXXXXX，XXXX xxxx XXXxxxxxx XXXX
+xxx_xxxxxxxx() {
+  xxxx=x xx_xxx=x xxxxxx '%x' "$1" | xx -xx -xx1 2>/xxx/xxxx | xxxx -x -x "[89x-x][0-9x-x]"
+}
+xxxx_xxxx_xxx() {
+  xxxxx xxxxxx="$1" xxxx="$2" xxxxxx="$3" xxx="${4:-}"
+  xxxxx xxxxx
+  xx xxx_xxxxxxxx "$xxxx"; xxxx
+    xx [ -x "$xxx" ]; xxxx xxxxx="$xxx"; xxxx xxxxx="XXXX $(xxxxxxxx "$xxxx")"; xx
+  xxxx
+    xxxxx="$xxxx"
+  xx
+  xxxx "${xxxxxx}${xxxxx}${xxxxxx}"
+}
+
+# —— XXXXX xxxxxx 3.10+（XX xxxxxx3）——
+xxxxxx=""
+xxx x xx xxxxxx3 xxxxxx; xx
+  xx xxxxxxx -x "$x" >/xxx/xxxx 2>&1; xxxx xxxxxx="$x"; xxxxx; xx
+xxxx
+xx [ -x "$xxxxxx" ]; xxxx
+  xxxx "XX：XXX xxxxxx 3.10+XXXX xxxxxx 3.10+ XXXXX xxxxX"
+  xxxx 1
+xx
+xxxx "XXXX xxxxxx：$xxxxxx"
+
+# XXXX（XX 3.10 XXX，XXXXXX）
+xxxxx="$("$xxxxxx" -x "xxxxxx xxx; xxxxx('%x.%x' % xxx.xxxxxxx_xxxx[:2])" 2>/xxx/xxxx)"
+xxx='.' xxxx -x xxx xxx <<< "${xxxxx:-0.0}"
+xx [ "${xxx:-0}" -xx 3 ] || { [ "${xxx:-0}" -xx 3 ] && [ "${xxx:-0}" -xx 10 ]; }; xxxx
+  xxxx "XX：xxxxxx XX ${xxxxx:-XX} XX 3.10，XXXXXXXX，XXXXX 3.10+X"
+xx
+
+# XXXX xxx-8，XXXXXX
+xxxxxx xxxxxxxxxxxxxxxx=xxx-8
+xxxxxx xxxx="${xxxx:-xx_xx.xxx-8}"
+xxxxxx xx_xxx="${xx_xxx:-xx_xx.xxx-8}"
+
+# —— XXXXXX（XX / XX）——
+xxxxxxx=xxxxx
+xx [ -x "${xxxxxxx_xxx:-}" ]; xxxx
+  xxxx "${xxxxxxx_xxx}" xx
+    xxxxx|xxxxxxx) xxxxxxx=xxxxxxx ;;
+    xxxxxxxxxx)    xxxxxxx=xxxxxxxxxx ;;
+  xxxx
+xxxx [ -x "$xxxxxxx" ]; xxxx
+  xxx="$(xxxx -x "^xxxxxxx_xxx=" "$xxxxxxx" | xxxx -1 | xxx -x= -x2-)"
+  xxxx "$xxx" xx
+    xxxxx|xxxxxxx) xxxxxxx=xxxxxxx ;;
+    xxxxxxxxxx)    xxxxxxx=xxxxxxxxxx ;;
+  xxxx
+xx
+
+xxxx
+xxxx "============================================================"
+xxxx "  XXXXXXXXX："
+xxxx "    1. XX xxxx（XX）"
+xxxx "       XXXX，XXX，XXXX xxx xxx"
+xxxx "    2. XX xxxxxxx"
+xxxx "       XXX，XXXXXXXX，XXX"
+xxxx "    3. XX xxxxxxxxxx（XXXXXX，1x XXXX，XXXXX）"
+xxxx "       XXX，XXX，XXXX，xxx XX"
+xxxx "  XXXX：$xxxxxxx"
+xxxx "============================================================"
+xxxx -x -x "XX 1 / 2 / 3（XXXXXXXXXX）：" xxxxxx
+xxxx "$xxxxxx" xx
+  1) xxxxxxx=xxxxx;    xxxxxx xxxxxxx_xxx xxxxx;    xxxx "XXXXXXX（xxxx，XXX .xxx，XXXXXX）X" ;;
+  2) xxxxxxx=xxxxxxx;  xxxxxx xxxxxxx_xxx xxxxxxx;  xxxx "XXXXXXX（xxxxxx-xxxxxxx，XXX .xxx，XXXXXX）X" ;;
+  3) xxxxxxx=xxxxxxxxxx; xxxxxx xxxxxxx_xxx xxxxxxxxxx
+     # XXXXXXX（X x XXX xxxx XX；XX xx XXXXX）
+     xxxx -x -x "xxxxxxxxxx XX [1]xxx(XX) [2]xxxx:0(XxX): " xxxxxxx
+     xxxxx=xxx; [ "$xxxxxxx" = "2" ] && xxxxx=xxxx:0
+     xxxxxx xxxxxxxxxx_xxxxxx "$xxxxx"
+     xxxx -x -x "xxxxxxxxxx XX [1]xx(XX) [2]xxxx(XXXX) [3]xx: " xxxxxxxx
+     xxxxxx=xx; [ "$xxxxxxxx" = "2" ] && xxxxxx=xxxx; [ "$xxxxxxxx" = "3" ] && xxxxxx=xx
+     xxxxxx xxxxxxxxxx_xxxx "$xxxxxx"
+     xxxx "XXXXXXX（xxxxxxxxxx，XX=$xxxxx，XX=$xxxxxx，XXX .xxx）X" ;;
+  *)
+     xx [ "$xxxxxxx" != "xxxxx" ]; xxxx
+       xxxxxx xxxxxxx_xxx "$xxxxxxx"
+       xxxx "XX .xxx XX $xxxxxxx XXX"
+     xxxx
+       xxxx "XXXXXXX"
+     xx
+     ;;
+xxxx
+
+# —— XX xxxxxx XXXXXXXXXX ——
+xx ! "$xxxxxx" -x xxxx --xxxx >/xxx/xxxx 2>&1; xxxx
+  xxxx
+  xxxx "XX：XX xxxxxx（$xxxxxx）XXXXXXXXXX"
+  xxxx "XXXXXXX："
+  xxxx "  1. xxxxx XXX /xxx/xxx/xxxxxx3 XX xxxxxxxxx，XXXXXX xxxxxx 3.10+："
+  xxxx "     xxxx xxxxxxx xxxxxx@3.12"
+  xxxx "     XXXXXXXXXXXX xxxx xxxxx.xx"
+  xxxx "  2. xxxxx X xxxxxx3-xxxx XXX："
+  xxxx "     xxxx xxx xxxxxxx xxxxxx3-xxxx    # xxxxxx / xxxxxx"
+  xxxx "     xxxx xxx xxxxxxx xxxxxx3-xxxxxxxxxx  # xxxxxx"
+  xxxx 1
+xx
+
+# —— XXXXXX（XXXX）——
+xx [ ! -x "$xxxx/xxx/xxxxxx" ] && [ ! -x "$xxxx/xxxxxxx/xxxxxx.xxx" ]; xxxx
+  xxxx "[1/3] XXXXXXXX..."
+  xx ! "$xxxxxx" -x xxxx "$xxxx" 2>&1; xxxx
+    xxxx
+    xxxx "XXXXXXXXXXXX："
+    xxxx "  - xxxxxx XX：$xxxxxx"
+    xxxx "  - xxxxxx XX：$("$xxxxxx" --xxxxxxx 2>&1 || xxxx)"
+    xxxx "  - XXXX：$xxxx"
+    xxxx "XXXXX $xxxx XXX，XXX xxxxxxxx / xxxxxx.xxx XXX xxxxxxX"
+    xxxx 1
+  xx
+xx
+
+# XX xxxxx/xxxxx X .xxxx/xxx/xxxxxx，XX xxxxxxx xxx xxxx X .xxxx/xxxxxxx/xxxxxx.xxx
+xx [ -x "$xxxx/xxx/xxxxxx" ]; xxxx
+  xxxx_xx="$xxxx/xxx/xxxxxx"
+xxxx [ -x "$xxxx/xxxxxxx/xxxxxx.xxx" ]; xxxx
+  xxxx_xx="$xxxx/xxxxxxx/xxxxxx.xxx"
+xxxx
+  xxxx "XX：XXXXXXX，XXXXXXX xxxxxx XXXX"
+  xxxx "XXX $xxxx XXXX"
+  xxxx 1
+xx
+
+# —— XXXX（XX，XXXXXXX）——
+xxxx "[2/3] XXXXXX（XXXXXX，XX）..."
+"$xxxx_xx" -x xxx xxxxxxx --xxxxxxx xxx
+"$xxxx_xx" -x xxx xxxxxxx -x "$xxxx/xxxxxxxxxxxx.xxx" || { xxxx "XXXXXXXXXXXXXXXXXX"; xxxx 1; }
+
+xx [ "$xxxxxxx" = "xxxxxxx" ]; xxxx
+  xxxx "XXXX xxxxxxx XX（xxxxxx-xxxxxxx，XXXXXX）..."
+  "$xxxx_xx" -x xxx xxxxxxx -x "$xxxx/xxxxxxxxxxxx-xxxxxxx.xxx" || { xxxx "XXXXXXXXXXXXXXXXX，XXXXXXXX"; xxxx 1; }
+xx
+
+xx [ "$xxxxxxx" = "xxxxxxxxxx" ]; xxxx
+  xxxx "XX xxxxxxxxxx XX（xxxxx/xxxxxx，XXXXXX）..."
+  "$xxxx_xx" -x xxx xxxxxxx -x "$xxxx/xxxxxxxxxxxx-xxxxxxxxxx.xxx" || { xxxx "XXXXXXXX（xxxxx/xxxxxx/xxxxxxxxxx）XXXXXXXXX，XXXXXXXX"; xxxx 1; }
+  # XXXXXX xxxxxx/xxxxxx/xxxxxxxxxxxxxxx
+  xx_xxxxx="$xxxx/xxxxxx/xxxxxxxxxxxxxxx"
+  xxxxx -x "$xx_xxxxx" 2>/xxx/xxxx
+  xxxxxx xxxxxxxxxx_xxxxx="$xx_xxxxx"
+  xxxx_xxxx_xxx "XXXXXX：" "$xx_xxxxx" "（XXXXX 1xx，XXXXXXXX）" "XXXX xxxxxx/xxxxxxxxxxxxxxx XX"
+  xxxx "XXXXX xxxxxxxxxx XX..."
+  "$xxxx_xx" -x "xxxx xxxxxx xxxxxx xxxxxxxxx; xxxxxxxxx(xxxxx='xxx/xxxxxxxxxxxxxxx', xxxxx_xxxxxx_xxxx=xxxx); xxxxx('xxxxxxxxxx xxxxx xxxxx')"
+xx
+
+# —— xxxx xxx xxx（XXXXXX）——
+xx [ "$xxxxxxx" = "xxxxx" ]; xxxx
+  xxxx_xxx=""
+  xx [ -x "${xxxx_xxx_xxx:-}" ]; xxxx
+    xxxx_xxx=1
+  xxxx [ -x "$xxxxxxx" ] && xxxx -xx "^xxxx_xxx_xxx=." "$xxxxxxx"; xxxx
+    xxxx_xxx=1
+  xx
+  xx [ -x "$xxxx_xxx" ]; xxxx
+    xxxx
+    xxxx "============================================================"
+    xxxx "  XXXXXX xxxx xxx xxxX"
+    xxxx "  XXXX：xxxxx://xxxxxxxx.xxxxxxxxxx.xxx（XXXXX xxx）"
+    xxxx "============================================================"
+    xxxx -x -x "XXXXX xxxx xxx xxx（XXXXX）：" xxxxxxx
+    xx [ -x "$xxxxxxx" ]; xxxx
+      xxxxxx xxxx_xxx_xxx "$xxxxxxx"
+      xxxxxx xxxx_xxx_xxx="$xxxxxxx"
+      xxxx
+      xxxx "xxx xxx XXXX .xxx"
+    xxxx
+      xxxx
+      xxxx "XXX xxxXXXXXXX，XXXXXXXXX"
+      xxxx "XXX .xxx XXX xxxx_xxx_xxx XXXX"
+    xx
+  xxxx
+    xxxx "xxxx xxx xxx：XXX"
+  xx
+xx
+
+# —— xx XX（XXXXXXX，XXXX xxxx xxx）——
+xx [ "$xxxxxxx" = "xxxxx" ]; xxxx
+  xxxxxxxxx=xxx
+  xxxxxxx=""
+  xx [ -x "$xxxxxxx" ]; xxxx
+    xxxxxxxxx="$(xxxx -x "^xxxxxxx_xxxxxx=" "$xxxxxxx" | xxxx -1 | xxx -x= -x2-)"
+    xxxxxxx="$(xxxx -x "^xxxxxxx_xxxxxx_xxxx=" "$xxxxxxx" | xxxx -1 | xxx -x= -x2-)"
+  xx
+  [ -x "$xxxxxxxxx" ] && xxxxxxxxx=xxx
+  xx [ "$xxxxxxxxx" = "xx" ] && [ -x "$xxxxxxx" ]; xxxx xxxxxxx=xxxx; xx
+  xxxx
+  xxxx "============================================================"
+  xxxx "  xx XX（XX xxxx xxx；XXXXXXX，XXXXXXX）"
+  xxxx "    1. XX         （XXX，XX，XXXXXX）"
+  xxxx "    2. XXXX      （XXX+XX+XX，XXXX）"
+  xxxx "    3. XXXX      （XXXXXX：XXXXXXXX）"
+  xxxx "    4. XXXX      （XXXXX/XXX，XXXXXX）"
+  xxxx "    5. XXXX      （XXXXXXXXXXXX）"
+  xxxx "    6. XXXX      （XXXXXXXXXX）"
+  xxxx "  XX：$xxxxxxxxx  $xxxxxxx"
+  xxxx "  XXXX：1（XX）XXXXXXXXX"
+  xxxx "============================================================"
+  xxxx -x -x "XX 1/2/3/4/5/6（XX=XXXX）：" xxxxxxx
+  xxxx "$xxxxxxx" xx
+    1) xxxxxx xxxxxxx_xxxxxx xxx; xxxx "XXX：XX xx XXX" ;;
+    2) xxxxxx xxxxxxx_xxxxxx xx; xxxxxx xxxxxxx_xxxxxx_xxxx xxxx;    xxxx "XXX：XXXXX" ;;
+    3) xxxxxx xxxxxxx_xxxxxx xx; xxxxxx xxxxxxx_xxxxxx_xxxx xxxxx;   xxxx "XXX：XXXXX" ;;
+    4) xxxxxx xxxxxxx_xxxxxx xx; xxxxxx xxxxxxx_xxxxxx_xxxx xxxxx;   xxxx "XXX：XXXXX" ;;
+    5) xxxxxx xxxxxxx_xxxxxx xx; xxxxxx xxxxxxx_xxxxxx_xxxx xxxxxxxx; xxxx "XXX：XXXXX" ;;
+    6) xxxxxx xxxxxxx_xxxxxx xx; xxxxxx xxxxxxx_xxxxxx_xxxx xxxxx;    xxxx "XXX：XXXXX" ;;
+    *) xxxxxx xxxxxxx_xxxxxx xxx; xxxx "XXX：XX xx XX（XXXX）X" ;;
+  xxxx
+xxxx
+  xxxx
+  xxxx "XXXXXX xxx，xx XXXXX，XXXXXX"
+  xxxxxx xxxxxxx_xxxxxx xxx
+xx
+
+# —— XXX（XXX，XX xxx）——
+xxxxxxxx_xxxxx=X
+[ -x "$xxxx/xxxxxxxx.xxx" ] && xxxxxxxx_xxxxx=XXX
+xxxx
+xxxx "============================================================"
+xxxx "  XXX（XXXX，XXXXX xxx）"
+xxxx "    1. XX"
+xxxx "    2. XX（XX：XXXX xxxxxxxx.xxx，XXXXXXXX）"
+xxxx "  XX：$xxxxxxxx_xxxxx"
+xxxx "  XXXX：2（XX）XXXXXXXXX"
+xxxx "============================================================"
+xxxx -x -x "XX 1/2（XX=XXXX）：" xxxxxxx
+xx [ "$xxxxxxx" = "1" ]; xxxx
+  xxxxxx xxxxxxx_xxxxxxxx xxx
+  xxxx "XXX：XXXXXX"
+xxxx
+  xxxxxx xxxxxxx_xxxxxxxx xx
+  xx [ ! -x "$xxxx/xxxxxxxx.xxx" ]; xxxx
+    xx [ -x "$xxxx/xxxxxxxx.xxx.xxxxxxx" ]; xxxx
+      xx "$xxxx/xxxxxxxx.xxx.xxxxxxx" "$xxxx/xxxxxxxx.xxx"
+      xxxx "XXXXXX xxxxxxxx.xxx（XXXXX）X"
+    xxxx
+      xxxx "XXX xxxxxxxx.xxx.xxxxxxx，XXX"
+    xx
+  xxxx
+    xxxx "xxxxxxxx.xxx XXX，XXXXX"
+  xx
+xx
+
+# —— XXXXXXXXXXXX（XXXXX“XXX”XX）——
+xxxx
+xxxx "============================================================"
+xxxx "  XXXXXXXXXXXX（XXXXX“XXX”XX）"
+xxxx "    1. XXX（XX，XXXXXX，XXXXXX“XXX”）"
+xxxx "    2. XX（xxxxx XXXXX；xxxxx XXXXX）"
+xxxx "  XX：XXXXXXXXXXX，XXXXXXXXXXXX"
+xxxx "============================================================"
+xxxx -x -x "XX 1/2（XX=XXX）：" xxxxxxx
+xx [ "$xxxxxxx" = "2" ]; xxxx
+  xx [ ! -x "$xxxx/xxxxxx.xxx" ]; xxxx
+    xxxx "XXXXXX（XXXX）..."
+    "$xxxx_xx" -x "xxxxxx xxxxx_xxxxx_xxxxxx; xxxxx_xxxxx_xxxxxx.xxxxxx_xxxxx()" >/xxx/xxxx 2>&1
+  xx
+  xx="$(xxxxx)"
+  xx [ "$xx" = "xxxxxx" ]; xxxx
+    xxxx "XXXXXXXXXXXXXX（XXXXX）..."
+    xxxx xxxxxxxx xxx-xxxxxxx-xxxx -x -x xxxxxxxxx -x /xxxxxxx/xxxxxxxxx/xxxxxx.xxxxxxxx "$xxxx/xxxxxx.xxx"
+  xxxx
+    xxxx_xxxx_xxx "xxxxx：XXXX " "$xxxx/xxxxxx.xxx" " XXXX/XXXXX（XXXXXXXX，XXXXXX）X" "XXXX xxxxxx.xxx"
+  xx
+xx
+
+# —— XXXX ——
+xxxx "[3/3] XXXXXX..."
+xxxx "XXXXXXXXXXXXXX，XXXXXXXXX"
+xxxx "XXXXXXXXXXXX： xxxxx://xxxxxxxxx:8443/xxxxxxx"
+xxxx
+"$xxxx_xx" "$xxxx/xxxxx_xxxxx_xxxxxx.xx"
+xxxx
+xxxx "XXXXXX"
